be1dbf80a0e9ced84f22c4a2cf081b8ce5b6ffb1
Aurora Daniel <noahd29@parkschool.net>
2026-01-20 03:35:53 -0500

    crt shader bullshit

--- Global/driver.js
+++ Global/driver.js
@@ -9,10 +9,12 @@
 var fMonoton;
 var fIconicIonic;
 
-var canvas;
-
+var crt;
 async function setup() {
-	canvas = createCanvas(480*1.5, 270*1.5);
+	createCanvas(480*1.5, 270*1.5);
+	crt = await loadStrings('../crt.frag');
+	crt = crt.join('\n')
+	crt = createFilterShader(crt);
 
 	// See constants.js:42
 	W = width/NATIVE_RESOLUTION.width;

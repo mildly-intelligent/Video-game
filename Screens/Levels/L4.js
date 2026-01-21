63f33bb986d2b8fff30c411a992b07781a27ca44
Aurora Daniel <noahd29@parkschool.net>
2026-01-21 13:47:19 -0500

    Added name to level 5

--- Screens/Levels/L4.js
+++ Screens/Levels/L4.js
@@ -3,7 +3,7 @@
 // 01-16-26
 /// <reference path="/home/aurora/.vscode/extensions/samplavigne.p5-vscode-1.2.16/p5types/global.d.ts" />
 
-var lv4 = new Level(4, setupL4a, setupL4b, drawL4a, drawL4b, "test").register();
+var lv4 = new Level(4, setupL4a, setupL4b, drawL4a, drawL4b, "Walls").register();
 if (opts.debug.unlock_all_levels) lv4.status |= LEVEL_STATUS.UNLOCKED;
 
 var bSquareL4 = false;
